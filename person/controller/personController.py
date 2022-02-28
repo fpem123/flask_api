83d704ac32c270b49b2b4c6bde0b2a75e833bd69
Lee Hoseop <ghtjq3909@naver.com>
2022-02-28 22:21:48 +0900

    person row API

--- person/controller/personController.py
+++ person/controller/personController.py
@@ -1,7 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, request
 import person.model.personModel as model
 
 person_api = Blueprint('person_api', __name__)
+
+@person_api.route("/", methods=["GET"])
+def visit_search():
+    try:
+        column = request.args.get('column', type=str, default="all")
+        keyword = request.args.get('keyword', type=str, default="")
+        start = request.args.get('start', type=int, default=0)
+        many = request.args.get('many', type=int, default=10)
+    except Exception as e:
+        return {"result": "error"}, 400
+        
+    try:
+        res = model.person_row_search(column, keyword, start, many)
+
+        return {"result": res}, 200
+    except Exception as e:
+        print(e)
+        return {"result": "error"}, 400
 
 @person_api.route("/all", methods=["GET"])
 def person_count():
--- person/domain/personDomain.py
+++ person/domain/personDomain.py
@@ -5,7 +5,7 @@ db = flask_sqlalchemy.SQLAlchemy()
 class Person(db.Model):
     __tablename__ = 'person'
     person_id = db.Column(db.Integer, primary_key=True)
-    gender_concept_id = db.Column(db.Integer, nullable=False)
-    birth_datetime = db.Column(db.Integer, nullable=False)
-    race_concept_id = db.Column(db.Integer, nullable=False)
-    ethnicity_concept_id = db.Column(db.Integer, nullable=False)
+    gender_concept_id = db.Column(db.Integer, db.ForeignKey("concept.concept.concept_id"))
+    birth_datetime = db.Column(db.Date, nullable=False)
+    race_concept_id = db.Column(db.Integer, db.ForeignKey("concept.concept.concept_id"))
+    ethnicity_concept_id = db.Column(db.Integer, db.ForeignKey("concept.concept.concept_id"))
