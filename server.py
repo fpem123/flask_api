f25e8f179d387eb3ec5dc014a66989b754629b28
Lee Hoseop <ghtjq3909@naver.com>
2022-02-28 16:20:21 +0900

    add person api base

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from person.controller import personController
 
 app = Flask(__name__)
+
+app.register_blueprint(personController.person_api, url_prefix="/person")
 
 if __name__ == '__main__':
     app.run(debug=True)
