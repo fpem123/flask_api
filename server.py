cd563332f6bb29908a94a0048c0277240b3110fc
Lee Hoseop <ghtjq3909@naver.com>
2022-02-28 16:30:14 +0900

    add db access

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from person.controller import personController
 
+db_host = input("[DB HOST] : ")
+db_port = input("[DB port] : ")
+db_name = input("[DB name] : ")
+db_user = input("[DB user] : ")
+db_password = input("[DB password] : ")
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 
 app.register_blueprint(personController.person_api, url_prefix="/person")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
